8b42ad0bb68ac07b9c0b2c8a86378dece1130b36
Synthia Rahman <synthiarahman59@gmail.com>
2025-11-26 13:59:40 +0600

    Add files via upload

---
+++ src/mypackage/STATICBLOCK.java
@@ -0,0 +1,15 @@
+package mypackage;
+
+public class STATICBLOCK {
+	static int x;
+	static double b;
+	static {
+		x = 10;
+		b = 20.2;
+	}
+static void display()
+{
+	System.out.println(x+b);
+	
+}
+}
